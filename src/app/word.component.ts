b8a5b81fc5b2d539aed8322e6dce99ab8228b626
Tran Minh Tien <32189164+tientm96@users.noreply.github.com>
2018-07-10 23:20:40 +0700

    component: angular.io/api/core/Component

--- src/app/word.component.ts
+++ src/app/word.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-    template: '<h3>this is world component</h3>',
-    selector: 'app-world' //tạo tên thẻ của component khi gọi trong file app.component.html: <app-world></app-world>
+    //vào https://angular.io/api/ tại ô Filter search tìm Component, chọn @Component sẽ ra link https://angular.io/api/core/Component
+    //Theo đó, styles: là mảng nên phải [].
+    //Vào đó xem định dạng để làm cho đúng theo từng cái.
+
+    // template: '<h3>this is world component</h3>',
+    templateUrl: './word.component.html', //Dạng string bt. File Url thì gọi đường dẫn chứ ko chèn trực tiếp.
+    selector: 'app-world', //tạo tên thẻ của component khi gọi trong file app.component.html: <app-world></app-world>
+    
+    // để chuỗi được xuống nhiều dòng thì đặt chuỗi trong `` (trên phím Tab)
+    
+    // styles: [`  
+    //     h3 {
+    //         color: red;
+    //     }
+    // `]
+
+    styleUrls: ['./word.component.css'], //Dạng mảng. file Url thì gọi link, chứ ko chèn trực tiếp.
 })
 
 export class WorldComponent {}
