d55835d5a93c59e65056d1a53848b1041b62e0d8
DESKTOP-FQII8RQ\Tien's PC <tien.tran@newoceaninfosys.com>
2018-07-12 17:05:38 +0700

    Add item to array with *ngFor

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from "@angular/core";
 })
 export class WordsComponent implements OnInit {
 
+  //tạo ra các biến rỗng, để bên html sử dụng [(ngModel)] để gắn dữ liệu cho các biến này, mỗi lần nhập vào nó tự gắn dl.
+  newEn = '';
+  newVn = '';
+
+
+
   //thêm mảng object để dùng ngFor
   arrWords = [
     { id: 1, en: "action", vn: "hành động", memorized: true },
@@ -21,4 +27,21 @@ export class WordsComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  //hàm thêm phần tử vào mảng.
+  //nếu muốn thêm vào cuối thì .push, còn muốn thêm vào đầu thì .unshift
+  addWord(){
+    this.arrWords.unshift({
+      id: this.arrWords.length + 1,
+      en: this.newEn,   //this.newEn là gọi lên thuộc tính newEn='' ở trên. Thuộc tính này đc gắn với [(ngModel)] nên sẽ nhận đc các dl nhập vào.
+      vn: this.newVn,
+      memorized: false
+    });
+
+    //sau khi add vào thì xóa tráng lại các ô. Vì newEn đc gắn với [(ngModel)], nên dl 2 cái sẽ ăn theo với nhau. 
+    //lúc này chỉ cần ta cho newEn, newVn về lại rỗng thì lập tức [(ngModel)] cũng thành rỗng và ô nhập vào sẽ bị rỗng theo.
+    this.newEn = '';
+    this.newVn = '';
+  }
+
 }
