61bca66697426a709f3a331b81ec0f747860f624
Tran Minh Tien <32189164+tientm96@users.noreply.github.com>
2018-07-15 17:15:20 +0700

    ngForm-lay tat ca input cua form gop thanh 1 object

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 
 
+/*Hầu hết ứng dụng thu thập thông tin người dùng qua các form. Angular hỗ trở xử lý form input rất tốt.
+Đã học cách xử lý tập tin qua input bằng two-way binding, event binding.
+
+Tuy nhiên đối với các form nhập liệu lớn hơn, thì cách thu thập thông tin qua từng input sẽ gặp khó khăn.
+=>giải pháp: gộp các input đó thành các form để có sử dụng tốt hơn.
+
+Bài toán này: ta tạo ra 2 input và gộp 2 input đó lại thành 1 form. (xem trong file html)
+-----------------------------------
+Bài 44: Sử dụng ngForm để thực hiện bài toán trên (Qua html xem giải thích)
+
+
+*/
+
 export class SignInComponent implements OnInit {
   email = ''; //tạm thời rỗng, [(ngModel)] sẽ auto gắn giá trị cho nó.
   password = '';
@@ -16,8 +29,15 @@ export class SignInComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit(){
-      console.log(this.email, this.password);
+  // onSubmit(){
+  //     console.log(this.email, this.password);
+  // }
+
+  //ở đây formSignIn chỉ là 1 tham số hàm bình thường thôi, muốn đặt tên gì cũng đc.
+  //tham số ở đây để nhận tất cả input truyền từ ngForm(trong html) về, bằng cách gọi hàm này trong thẻ form trong html(qua đó mà xem).
+  onSubmit(formSignIn){
+      console.log(formSignIn.value); //.value có giải thích bên phần cuối giải thích của html.
   }
+
 }
 
