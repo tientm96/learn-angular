10766a95561f928410603e52be58300cce4ad74b
DESKTOP-FQII8RQ\Tien's PC <tien.tran@newoceaninfosys.com>
2018-07-17 15:30:33 +0700

    Custom reactive form validator

--- src/app/sign-up3/sign-up3.component.ts
+++ src/app/sign-up3/sign-up3.component.ts
@@ -33,7 +33,14 @@ export class SignUp3Component implements OnInit {
   ngOnInit() {
     this.formSignUp = this.fb.group({  
         /*ở đây sử dụng validator required: nếu lỗi required=true thì trả về invalid=true và valid=false, lúc đó bên html sẽ bị ẩn btn submit */
-        email: ['', Validators.required], 
+
+        //có thể .email(lỗi định dạng email), maxlenght, minlength... các loại validate xem trong https://angular.io/guide/reactive-forms 
+        // email: ['', Validators.required],  
+        // email: ['', Validators.email],  
+        // email: ['', gmailValidator],  //TEST VỚI VALIDATOR TỰ VIẾT BÊN DƯỚI
+
+        //TH sử dụng nhiều validator thì để trong []
+        email: ['', [Validators.email, Validators.required, gmailValidator] ],  
         password: '',
         subject: this.fb.group({
           nodeJS: false, 
@@ -47,4 +54,23 @@ export class SignUp3Component implements OnInit {
     console.log(this.formSignUp.value);
   }
 
+}
+
+//vào đây để xem các dạng validator có sẵn: https://angular.io/guide/reactive-forms 
+//Tự viết 1 validator: vd: email phải có chuỗi @gmail.com
+
+/*Bản chất validator là viết 1 function thôi. 
+-truyền vào tham số: đại diện cho formControl hoặc formGroup, đặt tên gì cũng đc nhưng nên đặt đúng với ý nghĩa của nó.
+nếu viết validator cho formControl thì đặt tên tham số truyền vào là formControl, ngược lại cho formGroup thì đặt formGroup
+
+*/
+
+function gmailValidator(formControl: FormControl){ //khai báo biến formControl với kdl FormControl, có thể (formControl) chỉ kb báo biến, ko cần kdl
+  if(formControl.value.includes('@gmail.com')){ //hàm includes() kiểm tra xem 1 chuỗi có chứa chuỗi khác ko
+    return null; //validator trả về null khi ko có lỗi gì 
+  }
+  
+  //ngược lại sẽ trả về có lỗi. Trả về object{ chức lỗi hiện ra trong này }
+  //tạo ra biến gmail gán cho nó gtri true, rồi return về biến gmail đó.
+  return { gmail : true};
 }
