2ed489d9eb1b16916407dc5b94acda669da414db
DESKTOP-FQII8RQ\Tien's PC <tien.tran@newoceaninfosys.com>
2018-07-24 18:08:43 +0700

    ng-content

--- src/app/folder-components/card.component.ts
+++ src/app/folder-components/card.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 
-/* ngContent giúp tạo ra những comp làm khung sườn, tức là làm những khung bao bên ngoài cho những comp khác.
+/* ngContent: phân vùng để đỗ dl vào. Dữ liệu đc đưa vào đi kèm mỗi khi gọi comp này ra. 
 
-phân vùng để đỗ dl vào. Dữ liệu đc đưa vào đi kèm mỗi khi gọi comp này ra. 
-
-<com này>dl đưa vào mỗi khi gọi nó ra ở comp khác</com này>
+vd:trong app.comp.html sẽ gọi comp này ra và truyền dl vào (NHƯNG KO PHẢI TRUYỀN THEO KIỂU @INPUT, MÀ LÀ TRUYỀN <comp>DL Ở GIỮA <comp> THẺ NTN)
+    
+<com này NẾU TRUYỀN THEO @INPUT THÌ PHẢI TRUYỀN TRONG NÀY>dl đưa vào mỗi khi gọi nó ra ở comp khác</com này>
 
 - Chỉ truyền luôn vào class chính, truyền vào 1 chỗ :
 Ở đây ta truyền <ng-content></ng-content> trong class="card" là class tạo 1 khung bọc xung quanh.
@@ -26,14 +26,33 @@ vd: bên app.comp.html: <p class="card-body">Khoa phạm: nhiều chỗ-vào bod
   selector: "app-card",
   template: `
         <div class="card">
+            <h1>ng-content nhận những cái ko đk nào cả</h1>
             <ng-content></ng-content>
-            
-            <div class="header">
-                <ng-content select=".card-header"></ng-content>
-            </div>
-            <div class="body">
-                <ng-content select=".card-body"></ng-content>
-            </div>
+            <br>
+
+            <h1>ng-content nhận theo đk class</h1>
+            <ng-content select=".card-header"></ng-content>
+            <br>
+            <ng-content select=".card-body"></ng-content>
+            <br>
+            <ng-content select=".myClass"></ng-content>
+            <br>
+
+            <h1>ng-content nhận theo đk thẻ h4</h1>
+            <ng-content select="h4"></ng-content>
+            <br>
+
+            <h1>ng-content nhận theo đk thẻ (div myAttr), div hay p hay h đều đc </h1>
+            <ng-content select="[myAttr]"></ng-content>
+            <br>
+
+            <h1>ng-content nhận theo đk thẻ (span course="angular6"), span hay div hay p đều đc</h1>
+            <ng-content select="[course=angular6]"></ng-content>
+            <br><br>
+
+            <h1>Multi Attribute: ng-content nhận theo đk thẻ (p handsome demo) </h1>
+            <ng-content select="[handsome][demo]"></ng-content>
+            <br>
 
         </div>
     `,
