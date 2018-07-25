d245ebe4334584936196a171847f3b4371e8e142
Tran Minh Tien <32189164+tientm96@users.noreply.github.com>
2018-07-26 00:25:02 +0700

    update: template reference variable - #a: luon nhan vao dang text - string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   nameComp = 'Tèo Nguyễn';
   ageComp = 18;
   
-  total = 0;
+  total : number= 0;
 
   public a : number;
   public b : number;
@@ -20,7 +20,23 @@ export class AppComponent {
     this.isDestroy = !this.isDestroy;
   }
 
-  amount(){
-    this.total = this.a + this.b;
+  //Nếu dùng ([ngModel])
+  // amount(){
+  //   this.total = this.a + this.b;
+
+  //   console.log(typeof this.a, typeof this.b);
+  // }
+  
+ 
+  //Nếu dùng template reference variable #a, #b
+  /*Chú ý: vì là biến template nên nó luôn nhận vào dạng text, dù ta có để input đầu vào là number đi chăng nữa.
+  Vậy nên template ref var luôn trả về dạng text ở mọi trường hợp. 
+
+  Muốn chuyển sang số thì ta phải ép kiểu: hoặc là Number(a) hoặc là parseInt(a)*/ 
+  amount(a , b){
+    console.log(typeof a, typeof b);
+
+    this.total = Number(a) + parseInt(b);
   }
+
 }
