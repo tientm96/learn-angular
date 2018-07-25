e2d1a047f398628979c2f32f19801c947c3a1102
DESKTOP-FQII8RQ\Tien's PC <tien.tran@newoceaninfosys.com>
2018-07-25 09:34:54 +0700

    update lifecycle hooks

--- src/app/folder-components/lifecycle-hooks/lifecycle-hooks.component.ts
+++ src/app/folder-components/lifecycle-hooks/lifecycle-hooks.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges, OnInit, DoCheck, OnDestroy, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, DoCheck, OnDestroy, SimpleChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle-hooks',
   templateUrl: './lifecycle-hooks.component.html',
   styleUrls: ['./lifecycle-hooks.component.css']
 })
-export class LifecycleHooksComponent implements OnChanges, OnInit, DoCheck, OnDestroy {
+export class LifecycleHooksComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   public products : any[] = [];
 
@@ -16,7 +16,7 @@ export class LifecycleHooksComponent implements OnChanges, OnInit, DoCheck, OnDe
   /* ko phải hook, nhưng đc gọi đầu tiên, thường dùng để tiêm các Dependency Injection như các Service.
   */
   constructor() { 
-    console.log('LifeCycle hook: Constructor()');
+    console.log('Constructor()');
   } 
 
 
@@ -33,7 +33,7 @@ export class LifecycleHooksComponent implements OnChanges, OnInit, DoCheck, OnDe
   Console.log và F12 ta thấy, sau mỗi lần input thay đổi thì: sẽ cho ra object total:simpleChanges với total là tên input nhận sự thay đổi,
     và object đó chứa đầy đủ các thuộc tính như trên. */ 
   ngOnChanges(simpleChanges: SimpleChanges) {
-    console.log('LifeCycle hook: ngOnChanges()');
+    console.log('ngOnChanges()');
     console.log(simpleChanges);
     this.previousTotal = simpleChanges.total.previousValue;
   }
@@ -44,7 +44,7 @@ export class LifecycleHooksComponent implements OnChanges, OnInit, DoCheck, OnDe
   Dùng để khởi tạo các giá trị, các thuộc tính đầu vào của comp.
   */
   ngOnInit() {
-    console.log('LifeCycle hook: ngOnInit()');
+    console.log('ngOnInit()');
     this.products = [
       {
         id: 1,
@@ -57,15 +57,40 @@ export class LifecycleHooksComponent implements OnChanges, OnInit, DoCheck, OnDe
 
   /*ngDoCheck: cứ mỗi lần gọi ngOnChanges() HOẶC ngOnInit() thì hàm ngDoCheck() sẽ đc gọi theo.  */
   ngDoCheck() {
-    console.log('LifeCycle hook: ngDoCheck()');
+    console.log('ngDoCheck()');
+  }
+
+
+  /*ngAfterContentInit, ngAfterContentChecked, ngAfterViewInit, ngAfterViewChecked:
+  là các hook bên trong ngDoCheck*/ 
+
+  /*ngAfterContentInit: Sử dụng ng-content để kiểm tra, sử dụng ContentChild (kiểu ElementRef) -template reference variable
+  Chỉ đc gọi 1 lần duy nhất. Chỉ dành cho component*/ 
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit()');
+  }
+  //Được gọi nhiều lần. Chỉ dành cho component.
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked()');
+  }
+
+
+  /*ngAfterViewInit: đc gọi sau khi khởi tạo view của comp và các child views.
+  Chú ý: template variable #tenbien cũng là sd child view.
+  Chỉ đc gọi 1 lần.*/
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit()');
+  }
+  //ngAfterViewChecked: đc gọi nhiều lần. Chỉ dành cho comp.
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked()');
   }
 
 
   /*Hàm này chỉ thực thi khi comp đang hiện rồi bị ẩn đi, mỗi lần ẩn comp đi là nó lại đc thực thi.
   Sử dụng ngIf kết hợp với biến isDestroy trong app.component.html giúp việc ẩn hiện comp đc thực hiện mỗi lần ấn button.*/ 
   ngOnDestroy() {
-    console.log('LifeCycle hook: ngOnDestroy()');
+    console.log('ngOnDestroy()');
   }
-
 
 }
